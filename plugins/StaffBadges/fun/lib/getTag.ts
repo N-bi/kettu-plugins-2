2dbce3fbe4f71f049fd8f40c34128b16e0796d08
Nabi <112969732+N-bi@users.noreply.github.com>
2026-04-05 21:44:50 +0300

    Update getTag.ts

--- plugins/StaffBadges/fun/lib/getTag.ts
+++ plugins/StaffBadges/fun/lib/getTag.ts
@@ -3,9 +3,11 @@ import { chroma, constants, i18n } from "@vendetta/metro/common";
 import { storage } from "@vendetta/plugin";
 import { rawColors } from "@vendetta/ui";
 
-const { Permissions } = constants;
-const { computePermissions } = findByProps("computePermissions", "canEveryoneRole");
-const GuildMemberStore = findByStoreName("GuildMemberStore");
+// Permissions
+const { Permissions } = constants
+const { computePermissions } = findByProps("computePermissions", "canEveryoneRole")
+
+const GuildMemberStore = findByStoreName("GuildMemberStore")
 
 export const BUILT_IN_TAGS = [
     i18n.Messages.AI_TAG,
@@ -14,15 +16,15 @@ export const BUILT_IN_TAGS = [
     i18n.Messages.SYSTEM_DM_TAG_SYSTEM,
     i18n.Messages.GUILD_AUTOMOD_USER_BADGE_TEXT,
     i18n.Messages.REMIXING_TAG
-];
+]
 
 interface Tag {
-    text: string;
-    textColor?: any;
-    backgroundColor?: any;
-    verified?: boolean | ((guild, channel, user) => boolean);
-    condition?: (guild, channel, user) => boolean;
-    permissions?: string[];
+    text: string
+    textColor?: any
+    backgroundColor?: any
+    verified?: boolean | ((guild, channel, user) => boolean)
+    condition?: (guild, channel, user) => boolean
+    permissions?: string[]
 }
 
 const tags: Tag[] = [
@@ -32,66 +34,65 @@ const tags: Tag[] = [
     },
     {
         text: "OWNER",
+        //backgroundColor: rawColors.ORANGE_345,
         condition: (guild, channel, user) => guild?.ownerId === user.id
     },
     {
         text: "ADMIN",
+        //backgroundColor: rawColors.RED_560,
         permissions: ["ADMINISTRATOR"]
     },
     {
         text: "STAFF",
+        //backgroundColor: rawColors.GREEN_345,
         permissions: ["MANAGE_GUILD", "MANAGE_CHANNELS", "MANAGE_ROLES", "MANAGE_WEBHOOKS"]
     },
     {
         text: "MOD",
+        //backgroundColor: rawColors.BLUE_345,
         permissions: ["MANAGE_MESSAGES", "KICK_MEMBERS", "BAN_MEMBERS"]
     },
     {
         text: "VC Mod",
+        //backgroundColor: "#059669#",
         permissions: ["MOVE_MEMBERS", "MUTE_MEMBERS", "DEAFEN_MEMBERS"]
     },
     {
         text: "Chat Mod",
+        //backgroundColor: "#7C3AED",
         permissions: ["MODERATE_MEMBERS"]
     }
-];
+]
 
 export default function getTag(guild, channel, user) {
-    let permissions;
+    let permissions
     if (guild) {
         const permissionsInt = computePermissions({
-            user,
+            user: user,
             context: guild,
             overwrites: channel?.permissionOverwrites
-        });
+        })
         permissions = Object.entries(Permissions)
             .map(([permission, permissionInt]: [string, bigint]) =>
                 permissionsInt & permissionInt ? permission : "")
-            .filter(Boolean);
+            .filter(Boolean)
     }
 
     for (const tag of tags) {
         if (tag.condition?.(guild, channel, user) ||
             (!user.bot && tag.permissions?.some(perm => permissions?.includes(perm)))) {
 
-            const roleColor = storage.useRoleColor
-                ? GuildMemberStore.getMember(guild?.id, user.id)?.colorString
-                : undefined;
-            const backgroundColor = roleColor ?? tag.backgroundColor ?? rawColors.BRAND_500;
-            const textColor = (roleColor || !tag.textColor)
-                ? (chroma(backgroundColor).get('lab.l') < 70 ? rawColors.WHITE_500 : rawColors.BLACK_500)
-                : tag.textColor;
+            let roleColor = storage.useRoleColor ? GuildMemberStore.getMember(guild?.id, user.id)?.colorString : undefined
+            let backgroundColor = roleColor ? roleColor : tag.backgroundColor ?? rawColors.BRAND_500
+            let textColor = (roleColor || !tag.textColor) ? (chroma(backgroundColor).get('lab.l') < 70 ? rawColors.WHITE_500 : rawColors.BLACK_500) : tag.textColor
 
             return {
                 ...tag,
                 textColor,
                 backgroundColor,
-                verified: typeof tag.verified === "function"
-                    ? tag.verified(guild, channel, user)
-                    : tag.verified ?? false,
+                verified: typeof tag.verified === "function" ? tag.verified(guild, channel, user) : tag.verified ?? false,
                 condition: undefined,
                 permissions: undefined
-            };
+            }
         }
     }
-}
