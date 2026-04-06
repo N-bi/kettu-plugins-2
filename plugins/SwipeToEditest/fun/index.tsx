1dec8137df0d10f33955aea160f8779262d36cc1
Nabi <112969732+N-bi@users.noreply.github.com>
2026-04-06 03:37:23 +0300

    Update index.tsx

--- plugins/SwipeToEditest/fun/index.tsx
+++ plugins/SwipeToEditest/fun/index.tsx
@@ -1,13 +1,17 @@
 // V2
 import { findByProps } from "@vendetta/metro";
 
-onLoad: () => {
-  const m1 = findByProps("swipeToEditIconUrl");
-  console.log("swipeToEditIconUrl module:", m1);
-  
-  const m2 = findByProps("swipe_edit_undo");
-  console.log("swipe_edit_undo module:", m2);
+export default {
+  onLoad: () => {
+    const m1 = findByProps("swipeToEditIconUrl");
+    console.log("swipeToEditIconUrl module:", m1);
+    
+    const m2 = findByProps("swipe_edit_undo");
+    console.log("swipe_edit_undo module:", m2);
 
-  const m3 = findByProps("canEditMessage");
-  console.log("canEditMessage module:", m3);
-}
+    const m3 = findByProps("canEditMessage");
+    console.log("canEditMessage module:", m3);
+  },
+
+  onUnload: () => {},
+};
