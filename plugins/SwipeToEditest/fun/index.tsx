1cedf436e9c881b9fae7d077051e2c1036dae009
Nabi <112969732+N-bi@users.noreply.github.com>
2026-04-06 03:05:56 +0300

    Update index.tsx

--- plugins/SwipeToEditest/fun/index.tsx
+++ plugins/SwipeToEditest/fun/index.tsx
@@ -1,1 +1,21 @@
+import { findByProps } from "@vendetta/metro";
+import { after } from "@vendetta/patcher";
 
+let patches = [];
+
+export default {
+  onLoad: () => {
+    const SwipeModule = findByProps("useIsMessageSwipeActionsEnabled");
+
+    if (SwipeModule) {
+      patches.push(
+        after("useIsMessageSwipeActionsEnabled", SwipeModule, () => true)
+      );
+    }
+  },
+
+  onUnload: () => {
+    for (const p of patches) p();
+    patches = [];
+  },
+};
